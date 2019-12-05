265b6aa460a64a5aa0278c9317dbf91ca89c137a
bebee4java <grsong.cn@gmail.com>
2019-12-05 17:15:34 +0800

    try flink table api:word count

---
+++ src/main/java/os/dt/try1/flink/FlinkTableWordCount.scala
@@ -0,0 +1,29 @@
+package os.dt.try1.flink
+
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api.scala._
+
+object FlinkTableWordCount {
+
+
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val tEnv = BatchTableEnvironment.create(env)
+
+
+    val input = env.fromElements(WC("hello", 1), WC("hello", 1), WC("ciao", 1))
+    val expr = input.toTable(tEnv)
+    val result = expr
+      .groupBy('word)
+      .select('word, 'frequency.sum as 'frequency)
+      .filter('frequency === 2)
+      .toDataSet[WC]
+
+    result.print()
+  }
+
+  case class WC(word: String, frequency: Long)
+}
+
+
+
